04bc013fad4db81ce6e47139c1f2baec1ec63f67
Andreas Limber <andreas.limber@gmail.com>
2024-09-01 21:58:49 +0200

    working

--- bm-backend/app/models.py
+++ bm-backend/app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import ARRAY
 from app.db import Base

eed256e642885d6f4f5adde2ac6a0c500312f79a
Andreas Limber <andreas.limber@gmail.com>
2024-09-02 19:28:58 +0200

    syntax fixes

--- bm-frontend/src/components/Home.js
+++ bm-frontend/src/components/Home.js
@@ -11,7 +11,7 @@ function Home() {
             <p className="text-lg mb-6 text-center text-gray-600">Login with your Google account to get started.</p>
             <button 
                 onClick={handleLogin} 
-                class="confirm_button"
+                className="confirm_button"
             >
                 Login with Google
             </button>

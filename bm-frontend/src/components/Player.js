3f0324041a949b13d4de89c56302c23c32fb3e58
Andreas Limber <andreas.limber@gmail.com>
2024-09-13 11:54:32 -0400

    fixed warnings and errors

--- bm-frontend/src/components/Player.js
+++ bm-frontend/src/components/Player.js
@@ -3,32 +3,42 @@ import React from "react";
 
 function Player({ player }) {
   return (
-    <div class="p-5 w-80">
-      <h4 class="text-lg font-bold text-brown-700">{player.name}</h4>
-      <div class="md:block">
-        <div class="mr-10 flex items-baseline space-x-4">
-          <p class="rounded-md text-md font-medium">{player.race}</p>
-          <p class="rounded-md text-md font-medium">{player.role}</p>
+    <div className="p-5 w-80">
+      <h4 className="text-lg font-bold text-brown-700">{player.name}</h4>
+      <div className="md:block">
+        <div className="mr-10 flex items-baseline space-x-4">
+          <p className="rounded-md text-md font-medium">{player.race}</p>
+          <p className="rounded-md text-md font-medium">{player.role}</p>
         </div>
       </div>
 
-      <table class="table-auto w-full border-collapse text-xs">
+      <table className="table-auto w-full border-collapse text-xs">
         <thead>
-          <tr class="">
-            <th class="border border-gray-700 px-4 py-2">MA</th>
-            <th class="border border-gray-700 px-4 py-2">ST</th>
-            <th class="border border-gray-700 px-4 py-2">AG</th>
-            <th class="border border-gray-700 px-4 py-2">PA</th>
-            <th class="border border-gray-700 px-4 py-2">AV</th>
+          <tr className="">
+            <th className="border border-gray-700 px-4 py-2">MA</th>
+            <th className="border border-gray-700 px-4 py-2">ST</th>
+            <th className="border border-gray-700 px-4 py-2">AG</th>
+            <th className="border border-gray-700 px-4 py-2">PA</th>
+            <th className="border border-gray-700 px-4 py-2">AV</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td class="border border-gray-700 px-4 py-2">{player.stats[0]}</td>
-            <td class="border border-gray-700 px-4 py-2">{player.stats[1]}</td>
-            <td class="border border-gray-700 px-4 py-2">{player.stats[2]}</td>
-            <td class="border border-gray-700 px-4 py-2">{player.stats[3]}</td>
-            <td class="border border-gray-700 px-4 py-2">{player.stats[4]}</td>
+            <td className="border border-gray-700 px-4 py-2">
+              {player.stats[0]}
+            </td>
+            <td className="border border-gray-700 px-4 py-2">
+              {player.stats[1]}
+            </td>
+            <td className="border border-gray-700 px-4 py-2">
+              {player.stats[2]}
+            </td>
+            <td className="border border-gray-700 px-4 py-2">
+              {player.stats[3]}
+            </td>
+            <td className="border border-gray-700 px-4 py-2">
+              {player.stats[4]}
+            </td>
           </tr>
         </tbody>
       </table>
